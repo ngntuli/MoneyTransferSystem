9041693990ed558a73ff8012e21c36fbd3a0a8d2
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-14 00:40:21 +0200

    add logic to methods

--- src/main/java/com/ngntuli/bank/models/Transaction.java
+++ src/main/java/com/ngntuli/bank/models/Transaction.java
@@ -12,6 +12,14 @@ public class Transaction {
 	private TransferCategory transferCategory;
 	private Integer amount;
 
+	public Transaction(User recipient, User sender, Integer amount) {
+		this.id = UUID.randomUUID();
+		this.recipient = recipient;
+		this.sender = sender;
+		setTransferCategory(amount);
+		setAmount(sender, recipient, amount);
+	}
+
 	public UUID getId() {
 		return id;
 	}
@@ -40,15 +48,26 @@ public class Transaction {
 		return transferCategory;
 	}
 
-	public void setTransferCategory(TransferCategory transferCategory) {
-		this.transferCategory = transferCategory;
+	public void setTransferCategory(Integer amount) {
+		if (amount < 0) {
+			this.transferCategory = TransferCategory.DEBIT;
+		} else {
+			this.transferCategory = TransferCategory.CREDIT;
+		}
+
 	}
 
 	public Integer getAmount() {
 		return amount;
 	}
 
-	public void setAmount(Integer amount) {
+	public void setAmount(User sender, User recipient, Integer amount) {
+		if (sender.getBalance() < 0 || sender.getBalance() < amount) {
+			System.err.println("Transaction failed!");
+		} else {
+			sender.setBalance(sender.getBalance() - amount);
+			recipient.setBalance(recipient.getBalance() + amount);
+		}
 		this.amount = amount;
 	}
 
