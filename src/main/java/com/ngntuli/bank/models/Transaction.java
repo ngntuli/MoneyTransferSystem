b063e2e640130712f173a78b8f74bd4197eab7b0
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-13 17:47:35 +0200

    transaction entity

---
+++ src/main/java/com/ngntuli/bank/models/Transaction.java
@@ -0,0 +1,56 @@
+package com.ngntuli.bank.models;
+
+import java.util.UUID;
+
+public class Transaction {
+	public enum TransferCategory {
+		DEBIT, CREDIT
+	}
+
+	private UUID id;
+	private String recipient;
+	private String sender;
+	private TransferCategory transferCategory;
+	private Integer amount;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getRecipient() {
+		return recipient;
+	}
+
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public TransferCategory getTransferCategory() {
+		return transferCategory;
+	}
+
+	public void setTransferCategory(TransferCategory transferCategory) {
+		this.transferCategory = transferCategory;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+}
