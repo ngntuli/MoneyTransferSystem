69ed096ec614c37c5ff2407c500fa0c7896c548b
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-15 16:57:27 +0200

    updates transaction data access

---
+++ src/main/java/com/ngntuli/bank/daos/TransactionDao.java
@@ -0,0 +1,15 @@
+package com.ngntuli.bank.daos;
+
+import java.util.UUID;
+
+import com.ngntuli.bank.models.Transaction;
+
+public interface TransactionDao {
+	boolean add(Transaction transaction);
+
+	boolean remove(UUID id);
+
+	Transaction[] toArray();
+
+	int size();
+}
