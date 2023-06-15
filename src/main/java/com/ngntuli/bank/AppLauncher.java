2546aa5c91f96816970478aa4685224aac6f000b
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-16 01:26:01 +0200

    updates test

--- src/main/java/com/ngntuli/bank/AppLauncher.java
+++ src/main/java/com/ngntuli/bank/AppLauncher.java
@@ -1,11 +1,14 @@
 package com.ngntuli.bank;
 
+import com.ngntuli.bank.daos.TransactionDao;
+import com.ngntuli.bank.daos.TransactionDaoImpl;
 import com.ngntuli.bank.daos.UserDao;
 import com.ngntuli.bank.daos.UserDaoImpl;
 import com.ngntuli.bank.databases.UserIdsGenerator;
 import com.ngntuli.bank.models.Transaction;
 import com.ngntuli.bank.models.User;
 import com.ngntuli.bank.utilities.TransferCategory;
+import com.ngntuli.bank.validators.UserAlreadyExistsException;
 import com.ngntuli.bank.validators.UserNotFoundException;
 
 public class AppLauncher {
@@ -26,7 +29,7 @@ public class AppLauncher {
 		System.out.println("User by ID");
 		System.out.println("-".repeat(66));
 		for (int i = 1; i <= users.size(); i++) {
-			System.out.println(users.findUserByID(i));
+			System.out.println(users.find(i));
 		}
 		System.out.println("Last generated Id: " + UserIdsGenerator.getInstance().getId());
 		System.out.println("-".repeat(66));
@@ -42,63 +45,70 @@ public class AppLauncher {
 		System.out.println();
 	}
 
+	public static void createTransaction(TransactionDao transactionDao, User sender, User recipient, int amount) {
+		Transaction tr1 = new Transaction(sender, recipient, TransferCategory.DEBIT, amount);
+		transactionDao.add(tr1);
+		Transaction tr2 = new Transaction(sender, recipient, TransferCategory.CREDIT, amount);
+		transactionDao.add(tr2);
+	}
+
 	public static void main(String[] args) {
-		UserDao users = new UserDaoImpl();
-		System.out.println("Number of users - " + users.size());
-
-		users.addUser(new User("A", 100));
-		users.addUser(new User("B", 100));
-		users.addUser(new User("C", 100));
-		users.addUser(new User("D", 100));
-		users.addUser(new User("E", 100));
-
-		System.out.println("Number of users - " + users.size());
-
-		users.addUser(new User("F", 100));
-		users.addUser(new User("G", 100));
-		users.addUser(new User("H", 100));
-		users.addUser(new User("I", 100));
-		users.addUser(new User("J", 100));
-		users.addUser(new User("K", 100));
-		users.addUser(new User("L", 100));
-		users.addUser(new User("M", 100));
-
-		System.out.println("Number of users - " + users.size());
-
-		users.addUser(new User("N", 100));
-		users.addUser(new User("O", 100));
-		users.addUser(new User("P", 100));
-		users.addUser(new User("Q", 100));
-		users.addUser(new User("R", 100));
-		users.addUser(new User("S", 100));
-		users.addUser(new User("T", 100));
-		users.addUser(new User("U", 100));
-		users.addUser(new User("V", 100));
-		users.addUser(new User("W", 100));
-		users.addUser(new User("X", 100));
-		users.addUser(new User("Y", 100));
-		users.addUser(new User("Z", 100));
-
-		System.out.println("Number of users - " + users.size());
-		printUsersByIds(users);
-
-		System.out.println("Try to get User with non existing Name: SS");
-		if (users.findUserByName("SS") == null) {
-			throw new UserNotFoundException("Exception: User Not Found by Name: SS");
-		} else {
-			System.out.println("Get by name S - " + users.findUserByName("SS"));
-		}
-
-		System.out.println("Try to get User with non existing ID: 150");
-		if (users.findUserByID(150) == null) {
-			throw new UserNotFoundException("Exception: User Not Found by ID: 150");
-		} else {
-			System.out.println(users.findUserByID(150));
-		}
-
-		System.out.println("Try to add existing User: N");
-		users.addUser(new User("N", 100));
-		System.out.println("Number of users - " + users.size());
+//		UserDao users = new UserDaoImpl();
+//		System.out.println("Number of users - " + users.size());
+//
+//		users.addUser(new User("A", 100));
+//		users.addUser(new User("B", 100));
+//		users.addUser(new User("C", 100));
+//		users.addUser(new User("D", 100));
+//		users.addUser(new User("E", 100));
+//
+//		System.out.println("Number of users - " + users.size());
+//
+//		users.addUser(new User("F", 100));
+//		users.addUser(new User("G", 100));
+//		users.addUser(new User("H", 100));
+//		users.addUser(new User("I", 100));
+//		users.addUser(new User("J", 100));
+//		users.addUser(new User("K", 100));
+//		users.addUser(new User("L", 100));
+//		users.addUser(new User("M", 100));
+//
+//		System.out.println("Number of users - " + users.size());
+//
+//		users.addUser(new User("N", 100));
+//		users.addUser(new User("O", 100));
+//		users.addUser(new User("P", 100));
+//		users.addUser(new User("Q", 100));
+//		users.addUser(new User("R", 100));
+//		users.addUser(new User("S", 100));
+//		users.addUser(new User("T", 100));
+//		users.addUser(new User("U", 100));
+//		users.addUser(new User("V", 100));
+//		users.addUser(new User("W", 100));
+//		users.addUser(new User("X", 100));
+//		users.addUser(new User("Y", 100));
+//		users.addUser(new User("Z", 100));
+//
+//		System.out.println("Number of users - " + users.size());
+//		printUsersByIds(users);
+//
+//		System.out.println("Try to get User with non existing Name: SS");
+//		if (users.findUserByName("SS") == null) {
+//			throw new UserNotFoundException("Exception: User Not Found by Name: SS");
+//		} else {
+//			System.out.println("Get by name S - " + users.findUserByName("SS"));
+//		}
+//
+//		System.out.println("Try to get User with non existing ID: 150");
+//		if (users.findUserByID(150) == null) {
+//			throw new UserNotFoundException("Exception: User Not Found by ID: 150");
+//		} else {
+//			System.out.println(users.findUserByID(150));
+//		}
+//
+//		System.out.println("Try to add existing User: N");
+//		users.addUser(new User("N", 100));
+//		System.out.println("Number of users - " + users.size());
 
 //		Transaction[] transactions = new Transaction[3];
 //		transactions[0] = new Transaction(users.findUserByName("S"), users.findUserByName("P"), TransferCategory.DEBIT,
@@ -108,7 +118,65 @@ public class AppLauncher {
 //		transactions[2] = new Transaction(users.findUserByName("A"), users.findUserByName("E"), TransferCategory.DEBIT,
 //				10);
 //		printTransactions(transactions);
+//		System.out.println("User added: " + user.getName());
+//		throw new UserAlreadyExistsException("Exception: User already exist - " + user);
 
+		User user1 = new User("Charlotte", 1000);
+		User user2 = new User("Nkosinathi", 1000);
+
+		System.out.println("-".repeat(66));
+		System.out.println("Charlotte: " + user1 + "\nCharlotte list: \n" + user1.getTransactions());
+		System.out.println("Nkosinathi: " + user2 + "\nNkosinathi list: \n" + user2.getTransactions());
+		System.out.println("-".repeat(66));
+
+		System.out.println("\nLET'S CREATE TRANSACTIONS:");
+		TransactionDao transactionDao = new TransactionDaoImpl();
+		transactionDao.add(new Transaction(user1, user2, TransferCategory.DEBIT, 200));
+//		createTransaction(transactionDao, user1, user2, 200);
+//		createTransaction(transactionDao, user2, user1, 100);
+//		createTransaction(transactionDao, user2, user1, 150);
+//		createTransaction(transactionDao, user2, user1, 50);
+
+		System.out.println("Charlotte: " + user1 + "\nCharlotte list: \n" + user1.getTransactions());
+		System.out.println("Nkosinathi: " + user2 + "\nNkosinathi list: \n" + user2.getTransactions());
+
+//		public void setAmount(User sender, User recipient, Integer amount) {
+//			this.amount = amount;
+//			if ((this.transferCategory.equals(TransferCategory.DEBIT) && sender.getBalance() < amount)
+//					|| (this.transferCategory.equals(TransferCategory.CREDIT) && recipient.getBalance() < amount)) {
+//				System.err.println("Transaction [id=" + this.id + "] Failed!\n");
+//			} else if (transferCategory.equals(TransferCategory.DEBIT)) {
+//				sender.setBalance(sender.getBalance() - amount);
+//				recipient.setBalance(recipient.getBalance() + amount);
+//			} else {
+//				recipient.setBalance(recipient.getBalance() - amount);
+//				sender.setBalance(sender.getBalance() + amount);
+//			}
+//
+//		}
+
+//        System.out.println("\nREMOVE TRANSACTION:");
+//        Transaction[] transactions = user1.getList().toArray();
+//        Transaction toRemove = transactions[1];
+//        user1.getList().remove(toRemove.getUUID());
+//        user2.getList().remove(toRemove.getUUID());
+//        
+//        System.out.println("Charlotte: " + user1 + "\nCharlotte list: \n" + user1.getTransactions());
+//        System.out.println("Nkosinathi: " + user2 + "\nNkosinathi list: \n" + user2.getTransactions());
+//
+//        try {
+//            System.out.println("\nTRY TO REMOVE TRANSACTION WITH WRONG UUID:");
+//            user1.getList().remove(toRemove.getUUID());
+//        } catch (RuntimeException e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//        try {
+//            System.out.println("\nTRY TO REMOVE TRANSACTION WITH null UUID:");
+//            user2.getList().remove(null);
+//        } catch (RuntimeException e) {
+//            System.out.println(e.getMessage());
+//        }
 	}
 
 }
