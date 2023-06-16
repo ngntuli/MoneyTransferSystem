8a15e4569158742eea5e7692567288c5157588cd
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-16 23:10:50 +0200

    updates method names

--- src/main/java/com/ngntuli/bank/services/TransactionService.java
+++ src/main/java/com/ngntuli/bank/services/TransactionService.java
@@ -6,15 +6,9 @@ import com.ngntuli.bank.models.Transaction;
 import com.ngntuli.bank.models.User;
 
 public interface TransactionService {
-	boolean addUser(User user);
-
-	int getBalance(User user);
-
-	boolean createTransaction(int senderId, int recipientId, int amount);
-
-	boolean performTransfer(User sender, User recipient, int amount);
+	boolean createTransactionById(int senderId, int recipientId, int amount);
 
 	Transaction[] getTransactionsByUserId(int userId);
 
-	boolean removeTransaction(int userId, UUID id);
+	boolean removeTransactionByUserId(int userId, UUID id);
 }
