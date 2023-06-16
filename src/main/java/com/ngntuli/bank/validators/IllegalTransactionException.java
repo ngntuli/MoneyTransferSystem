7fc7f8d1cd47056e5b074dfc6bddd46284d4714b
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-16 17:30:45 +0200

    runtime illegal transaction exception

---
+++ src/main/java/com/ngntuli/bank/validators/IllegalTransactionException.java
@@ -0,0 +1,9 @@
+package com.ngntuli.bank.validators;
+
+public class IllegalTransactionException extends RuntimeException {
+
+	public IllegalTransactionException(String message) {
+		super(message);
+	}
+
+}
