8ae869e8f7dbbf2baa8820f5719f1d679587245a
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-19 23:00:18 +0200

    modifies add by user transaction method

--- src/main/java/com/ngntuli/bank/daos/TransactionDao.java
+++ src/main/java/com/ngntuli/bank/daos/TransactionDao.java
@@ -3,9 +3,11 @@ package com.ngntuli.bank.daos;
 import java.util.UUID;
 
 import com.ngntuli.bank.models.Transaction;
+import com.ngntuli.bank.models.User;
 
 public interface TransactionDao {
-	boolean addByTransaction(Transaction transaction);
+	boolean addByUserTransaction(User sender, Transaction senderTransaction, User recipient,
+			Transaction recipientTransaction);
 
 	boolean removeById(UUID id);
 
