edc74842052d8c37c4baf72b153e907eacb93a48
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-19 23:44:23 +0200

    implements a method

--- src/main/java/com/ngntuli/bank/services/UserServiceImpl.java
+++ src/main/java/com/ngntuli/bank/services/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ngntuli.bank.services;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.ngntuli.bank.daos.UserDaoImpl;
+import com.ngntuli.bank.models.Transaction;
 import com.ngntuli.bank.models.User;
 
 public class UserServiceImpl implements UserService {
@@ -26,6 +28,27 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public List<User> listAllUser() {
 		return userDao.findAllUsers();
+	}
+
+	@Override
+	public boolean removeTransactionById(UUID id) {
+		if (id != null) {
+
+			List<User> users = userDao.findAllUsers();
+			for (User user : users) {
+
+				List<Transaction> transactions = user.getTransactions();
+				for (Transaction transaction : transactions) {
+					if (transaction.getId().equals(id)) {
+						user.getTransactions().remove(transaction);
+						break;
+					}
+				}
+			}
+
+			return true;
+		}
+		return false;
 	}
 
 }
