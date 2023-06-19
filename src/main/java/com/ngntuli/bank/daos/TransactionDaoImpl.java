eed13c7007c8b78cf84575d4a970fc9fbae54a55
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-20 01:28:09 +0200

    updates a method

--- src/main/java/com/ngntuli/bank/daos/TransactionDaoImpl.java
+++ src/main/java/com/ngntuli/bank/daos/TransactionDaoImpl.java
@@ -1,22 +1,14 @@
 package com.ngntuli.bank.daos;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
 import com.ngntuli.bank.models.Transaction;
 import com.ngntuli.bank.models.User;
-import com.ngntuli.bank.utilities.TransferCategory;
 import com.ngntuli.bank.validators.TransactionNotFoundException;
 
 public class TransactionDaoImpl implements TransactionDao {
-	private UserDao userDao;
-	private List<User> users = new LinkedList<>();
-
-	public TransactionDaoImpl() {
-		userDao = new UserDaoImpl();
-		users = userDao.findAllUsers();
-	}
+	private UserDao userDao = new UserDaoImpl();
 
 	@Override
 	public boolean addByUserTransaction(User sender, Transaction senderTransaction, User recipient,
@@ -34,11 +26,16 @@ public class TransactionDaoImpl implements TransactionDao {
 	public boolean removeById(UUID id) {
 		if (id != null) {
 			boolean found = false;
+
+			List<User> users = userDao.findAllUsers();
 			for (User user : users) {
-				List<Transaction> list = user.getTransactions();
-				for (Transaction transaction : list) {
-					if (transaction.getId().equals(id)) {
-						list.remove(transaction);
+
+				List<Transaction> transactions = user.getTransactions();
+				for (Transaction transaction : transactions) {
+
+					UUID id2 = transaction.getId();
+					if (id2.equals(id)) {
+						transactions.remove(transaction);
 						found = true;
 						break;
 					}
