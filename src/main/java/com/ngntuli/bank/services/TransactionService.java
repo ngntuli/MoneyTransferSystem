2ed4d0938761d77353e5d57fdca8f385c6cd968f
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-20 21:04:10 +0200

    change return type of get transactions by user id

--- src/main/java/com/ngntuli/bank/services/TransactionService.java
+++ src/main/java/com/ngntuli/bank/services/TransactionService.java
@@ -1,5 +1,6 @@
 package com.ngntuli.bank.services;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.ngntuli.bank.models.Transaction;
@@ -8,7 +9,7 @@ import com.ngntuli.bank.models.User;
 public interface TransactionService {
 	void createTransactionByUserId(int senderId, int recipientId, int amount);
 
-	Transaction[] getTransactionsByUserId(int userId);
+	List<Transaction> getTransactionsByUserId(int userId);
 
 	boolean removeTransactionById(UUID id);
 }
