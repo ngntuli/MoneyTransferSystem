c792fac971df627adeef1ec1cdc1c9e33197b837
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-20 22:04:58 +0200

    illegal input data exception

---
+++ src/main/java/com/ngntuli/bank/validators/IllegalInputDataException.java
@@ -0,0 +1,9 @@
+package com.ngntuli.bank.validators;
+
+public class IllegalInputDataException extends RuntimeException {
+
+	public IllegalInputDataException(String message) {
+		super(message);
+	}
+
+}
