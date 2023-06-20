63d30419de710ea77f8db7ec6662a6630436318c
ngntuli <mr.ng.ntuli@gmail.com>
2023-06-20 22:28:48 +0200

    updates printing messages

--- src/main/java/com/ngntuli/bank/AppLauncher.java
+++ src/main/java/com/ngntuli/bank/AppLauncher.java
@@ -1,11 +1,15 @@
 package com.ngntuli.bank;
 
+import com.ngntuli.bank.databases.DataStore;
 import com.ngntuli.bank.views.View;
 
 public class AppLauncher {
 	private static View view = new View();
 
 	public static void main(String[] args) {
+		System.out.println("\n1. Loading Data...");
+		DataStore.getInstance().loadData();
+		System.out.println("\n2. Starting Application...");
 		view.menu();
 	}
 }
